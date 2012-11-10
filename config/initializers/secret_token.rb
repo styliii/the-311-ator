8bc15f092f8f71cc7117f17e94f34229bca4a401
Li Ouyang <li.ouyang@gmail.com>
2012-11-10 15:20:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+The311Ator::Application.config.secret_token = 'e4999710e340e34370468242672706b30fcbf41a8376b3f3336d4c96922bcf3ab77708e2ba2c6aa97d204c0307486bd181ad187ec3450fda643e9dd5b1c1b41d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+The311Ator::Application.config.session_store :cookie_store, key: '_the-311-ator_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# The311Ator::Application.config.session_store :active_record_store
